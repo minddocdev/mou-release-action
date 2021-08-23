9f443492d0874c5f751bdc2cb13684f1a30f07cc
Android 18 <57483221+minddocbot@users.noreply.github.com>
2021-08-23 23:13:20 +0200

    chore(): pin @types/node ^14.17.11 dev (#86)
    | datasource | package     | from     | to       |
    | ---------- | ----------- | -------- | -------- |
    | npm        | @types/node | 14.17.10 | 14.17.11 |
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- .pnp.js
+++ .pnp.js
@@ -43,7 +43,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@octokit/plugin-paginate-rest", "virtual:80bcb91a087ff4b248b3dad7606f95dff0dd8bfc050a19114604b6a694f57946c7c879aa3e66f037488a622f3abe53c788b66131c9ff0838a1318d83f1d9ce8e#npm:2.13.3"],
             ["@octokit/plugin-rest-endpoint-methods", "virtual:4378a32ce12a4ae9886fad0c3692664a6f4c4ab9a16443d423202e6122bf6d4a3efd171e035b95388d6948ab2c4ae05567c81abaf43c606554316232a8e8d263#npm:5.0.1"],
             ["@types/jest", "npm:26.0.24"],
-            ["@types/node", "npm:14.17.10"],
+            ["@types/node", "npm:14.17.11"],
             ["@types/prettier", "npm:2.2.3"],
             ["@types/semver", "npm:7.3.8"],
             ["@typescript-eslint/eslint-plugin", "virtual:4378a32ce12a4ae9886fad0c3692664a6f4c4ab9a16443d423202e6122bf6d4a3efd171e035b95388d6948ab2c4ae05567c81abaf43c606554316232a8e8d263#npm:4.29.2"],
@@ -1973,10 +1973,10 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           ],
           "linkType": "HARD",
         }],
-        ["npm:14.17.10", {
-          "packageLocation": "./.yarn/cache/@types-node-npm-14.17.10-f479af541c-3a0487d8c0.zip/node_modules/@types/node/",
+        ["npm:14.17.11", {
+          "packageLocation": "./.yarn/cache/@types-node-npm-14.17.11-1749a5f307-c52c533d83.zip/node_modules/@types/node/",
           "packageDependencies": [
-            ["@types/node", "npm:14.17.10"]
+            ["@types/node", "npm:14.17.11"]
           ],
           "linkType": "HARD",
         }]
@@ -7483,7 +7483,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@octokit/plugin-paginate-rest", "virtual:80bcb91a087ff4b248b3dad7606f95dff0dd8bfc050a19114604b6a694f57946c7c879aa3e66f037488a622f3abe53c788b66131c9ff0838a1318d83f1d9ce8e#npm:2.13.3"],
             ["@octokit/plugin-rest-endpoint-methods", "virtual:4378a32ce12a4ae9886fad0c3692664a6f4c4ab9a16443d423202e6122bf6d4a3efd171e035b95388d6948ab2c4ae05567c81abaf43c606554316232a8e8d263#npm:5.0.1"],
             ["@types/jest", "npm:26.0.24"],
-            ["@types/node", "npm:14.17.10"],
+            ["@types/node", "npm:14.17.11"],
             ["@types/prettier", "npm:2.2.3"],
             ["@types/semver", "npm:7.3.8"],
             ["@typescript-eslint/eslint-plugin", "virtual:4378a32ce12a4ae9886fad0c3692664a6f4c4ab9a16443d423202e6122bf6d4a3efd171e035b95388d6948ab2c4ae05567c81abaf43c606554316232a8e8d263#npm:4.29.2"],
